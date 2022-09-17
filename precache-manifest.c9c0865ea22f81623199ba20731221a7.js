aeb886e0a6e8a77f77898f0aa983d4087cdcb2c3
Code Sandbox <hello@codesandbox.io>
2022-09-17 14:14:18 +0000

    Deployment from Codesandbox.

---
+++ precache-manifest.c9c0865ea22f81623199ba20731221a7.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f2dc70f114525f3be9b0bf8939f3dd48",
+    "url": "https://rithymeth.github.io/csb-juyei9/index.html"
+  },
+  {
+    "revision": "b88238f9e6143eeed746",
+    "url": "https://rithymeth.github.io/csb-juyei9/static/js/2.72b2628c.chunk.js"
+  },
+  {
+    "revision": "cfc2e146b23de671563e",
+    "url": "https://rithymeth.github.io/csb-juyei9/static/js/main.83a16ac4.chunk.js"
+  },
+  {
+    "revision": "be74a8c037d863b8689b",
+    "url": "https://rithymeth.github.io/csb-juyei9/static/js/runtime-main.0761a1b9.js"
+  }
+]);
